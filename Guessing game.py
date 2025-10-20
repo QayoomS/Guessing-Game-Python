43350f74baaf2293595f0d8cbd5a5f47be9fe103
Qayoom Suroush <133157900+QayoomS@users.noreply.github.com>
2025-10-20 16:46:01 +0100

    Add comments to Guessing game.py
    Added comments to enhance code readability.

--- Guessing game.py
+++ Guessing game.py
@@ -1,8 +1,15 @@
+# import library:
 import random
+# We can chose the range of number that user can select:
 r_int = random.randint(1, 5)
+
+# Let give them 3 chance:
 guess_left = 3
+
+# A simple welcome message will be good:
 print('Welcome to the number guessing game')
 
+# using loops for the game:
 while guess_left >= 1:
     print('Guesses Left: ', guess_left)
     my_int = int(input('Guess a integer between 1 and 5:  '))
@@ -12,4 +19,5 @@ while guess_left >= 1:
         break
     else:
         print('Better Luck next time!')
+
 print('the game is over. Restart the program to play again!')
